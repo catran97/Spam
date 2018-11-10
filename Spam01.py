9d7e5fd90cd15819f8d103c43193e10b060408c5
EYagudin <43196756+EYagudin@users.noreply.github.com>
2018-11-10 11:29:37 +0500

    Текст сообщения берется из файла. Не работает кириллица, в процессе.

--- Spam01.py
+++ Spam01.py
@@ -1,14 +1,33 @@
-#spamgondola@gmail.com
+# -*- coding: utf-8 -*-
+#spamgondola@gmail.com Спамер
 #gondola2000
 
+#ykfgodnew@gmail.com Ярик
+#artur.tikhonov.97@mail.ru Артур
+#sanya.barckov@yandex.ru Саня
+#goodmorningsam66@gmail.com Жека
+
+from email.mime.text import MIMEText
+from email.header import Header
 import smtplib
-sender = "spamgondola@gmail.com"
-recipient = "goodmorningsam66@gmail.com"
-password = "gondola2000"
-subject = "TEST MESSAGE"
-text = "TEST 01 UAHHAHAH"
-smtp_server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-smtp_server.login(sender, password)
-message = "Subject: {}\n\n{}".format(subject, text)
-smtp_server.sendmail(sender, recipient, message)
-smtp_server.close()
+import io
+
+def sendMail(messagetext):
+    sender = "spamgondola@gmail.com"
+    password = "gondola2000"
+    recipient = "goodmorningsam66@gmail.com"
+    subject = "TEST MESSAGE"
+    text = MIMEText(messagetext, "plain", "utf-8")
+    smtp_server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    smtp_server.login(sender, password)
+    message = "Subject: {}\n\n{}".format(subject, text)
+    smtp_server.sendmail(sender, recipient, message)
+    smtp_server.close()
+
+mail_body = 'message.txt'
+with open(mail_body , encoding="utf8") as file:
+    messagetext = file.read()
+sendMail(messagetext)
+
+
+
