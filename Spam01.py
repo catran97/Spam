fb8b0d401f812dc2fdc8771a1cfb45a3f2206472
EYagudin <43196756+EYagudin@users.noreply.github.com>
2018-11-22 09:36:30 +0500

    Доп. данные для отправки теперь берутся из cfg файла

--- Spam01.py
+++ Spam01.py
@@ -7,26 +7,35 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from openpyxl import load_workbook
 
-sender = "spamgondola@gmail.com"
-password ="gondola2000"
+#Получение адреса и пароля отправителя, темы письма
+with open('config.cfg', encoding='utf-8-sig') as config:
+    for index, line in enumerate(config):
+        if index == 0:
+            serv_mail = line
+        elif index == 1:
+            serv_pswrd = line
+        elif index == 2:
+            serv_header = line
+            break
 
+#Отправление письма
 def sendMail(content, mail):
     message = MIMEMultipart('alternative')
-    message['Subject'] = "Spamgondola test 3"
-    message['From'] = sender
-    message['To'] = mail
+    message['Subject'] = serv_header
     message.add_header('Content-Type','text/html')
     text = MIMEText(content, 'html')
     message.attach(text)
-    mailserver = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    mailserver.login(sender, password)
-    mailserver.sendmail(message['From'], message['To'], message.as_string())
+    mailserver = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    mailserver.login(serv_mail, serv_pswrd)
+    mailserver.sendmail(serv_mail, mail, message.as_string())
     mailserver.quit()
 
+#Открытие файла письма
 mail_body = 'message.html'
-with open(mail_body , encoding="utf8") as file:
+with open(mail_body , encoding='utf8') as file:
     content = file.read()
 
+#Получение списка получателей
 ex_file = load_workbook('emails.xlsx')
 sheet = ex_file['email']
 column = sheet['D']
